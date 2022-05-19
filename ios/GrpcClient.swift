6ebb15feb609832ddac88615cce14cc5e023edc3
Hưng <hungphandinh92.it@gmail.com>
2022-05-20 01:02:20 +0700

    update grpc ios  function

--- ios/GrpcClient.swift
+++ ios/GrpcClient.swift
@@ -11,48 +11,49 @@ import React
 public class GrpcClient: RCTEventEmitter {
     public var emitter: RCTEventEmitter!
     var callback: BidirectionalStreamingCall<StreamingVoice_VoiceRequest, StreamingVoice_TextReply>!
+    public var encoder = JSONEncoder()
     override init() {
         super.init()
         emitter = self
     }
-    
+
     @objc open override func supportedEvents() -> [String]! {
         return ["open", "error", "completed", "message", "data"]
     }
-    
+
     @objc func onOpen() {
         emitter.sendEvent(withName: "open", body: [])
     }
-    
+
     @objc func onCompeleted() {
         emitter.sendEvent(withName: "completed", body: [])
     }
-    
+
     @objc func onError(message: String?) {
         emitter.sendEvent(withName: "error", body: message)
     }
-    
+
     @objc func onMessage(data: String?) {
         emitter.sendEvent(withName: "message", body: data)
     }
-    
-    
-    
+
+
+
     @objc(open:withB:)
     func open(host: String, port: Int) -> Void {
-        
+
         let group = PlatformSupport.makeEventLoopGroup(loopCount: 1)
         defer {
             try? group.syncShutdownGracefully()
         }
         do {
             let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-            
+
             let keepalive = ClientConnectionKeepalive(
                 interval: .seconds(15),
                 timeout: .seconds(10)
             )
-            
+
             let mChannel = try GRPCChannelPool.with(
                 target: .hostAndPort(host, port),
                 transportSecurity: .plaintext,
@@ -61,7 +62,7 @@ public class GrpcClient: RCTEventEmitter {
                 // Configure keepalive.
                 $0.keepalive = keepalive
             }
-            
+
             let callOptions = CallOptions(customMetadata: [
                 "channels": "1",
                 "rate": "16000",
@@ -70,8 +71,8 @@ public class GrpcClient: RCTEventEmitter {
                 "single-sentence": "True"
             ])
             let greeter = StreamingVoice_StreamVoiceClient.init(channel: mChannel, defaultCallOptions: callOptions)
-            
-            
+
+
             self.callback = greeter.sendVoice(callOptions: callOptions) { StreamingVoice_TextReply in
                 if StreamingVoice_TextReply.hasResult == false {
                     return
@@ -80,50 +81,51 @@ public class GrpcClient: RCTEventEmitter {
                 let resultFinal = StreamingVoice_TextReply.result.final
                 let lastResult = StreamingVoice_TextReply.result.hypotheses[0].transcript
                 print(resultFinal, lastResult)
-                
+
             }
             onOpen()
-            
-            
         }
         catch {
             //handle error
             print(error)
         }
-        
     }
-    
-    
+
+
     @objc
     func close() -> Void {
 //        self.callback.sendEnd()
     }
-    
+
     @objc(send:)
     func send(data: String) -> Void {
         var voice = StreamingVoice_VoiceRequest.with { StreamingVoice_VoiceRequest in
             print("Test")
         }
-        
+
         let data:Data = Data(base64Encoded: data, options: NSData.Base64DecodingOptions(rawValue: 0))!
         //        let data: Data = Data(base64Encoded: textSample, options: NSData.Base64DecodingOptions(rawValue: 0))!
-        
         voice.byteBuff = data
-        
+
         let event = self.callback.sendMessage(voice)
         event.whenComplete { result in
             print("complete", result)
+            onCompeleted()
         }
         event.whenSuccess { success in
             print("success", success)
+            encoder.outputFormatting = .prettyPrinted
+            let data = try encoder.encode(success)
+            onMessage(String(data: data, encoding: .utf8)!)
         }
         event.whenFailure { error in
             print("v", error)
+            onError(error)
         }
-        
+
     }
-    
-    
+
+
 }
 
 
